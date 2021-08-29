e8677fc69aa3fe96c728d6e524ad3522d43a8575
Nicholas Jefferis <jefferns@mail.uc.edu>
2021-08-29 03:06:34 -0400

    idk

--- contactform.php
+++ contactform.php
@@ -1,14 +1,14 @@
 <?php
 
 if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $mailFrom = $_POST['email'];
-    $message = $_POST['message'];
+    echo $name = $_POST['name'];
+    echo $subject = $_POST['subject'];
+    echo $mailFrom = $_POST['email'];
+    echo $message = $_POST['message'];
 
     $mailTo = "jefferns@mail.uc.edu";
     $headers = "From: ".$mailFrom;
-    $txt = "Automated Email from ".$name."at ".$mailFrom."\n\n".$message;
+    $txt = "Automated Email from ".$name."at ".$mailFrom.".\n\n".$message;
     
     mail($mailTo, $subject, $txt, $headers);
     header("Location: index.php?mailsend");
